8cbf6f0b99a9c77cf8c693e83d89f68fab02addd
wangben <wangben-1203@qq.com>
2025-06-19 10:02:14 +0800

    sharding:count

--- template.go
+++ template.go
@@ -2121,7 +2121,12 @@ func (c *_shardingCount) Do(ctx context.Context) (int64, map[{{.ShardingKeyType}
 			_conditionOpts = append(_conditionOpts, ConditionSharding(sharding))
 			cr := c.core.Count()
 			cr.writeDB = c.writeDB
-			count, err := cr.Tx(c.tx).QueryTx(c.qTx).Unscoped(c.unscoped).Scopes(c.scopes...).Where(_conditionOpts...).Do(ctx)
+			count, err := cr.Tx(c.tx).
+				QueryTx(c.qTx).
+				Unscoped(c.unscoped).
+				Scopes(c.scopes...).
+				Where(_conditionOpts...).
+				Do(ctx)
 			if err != nil {
 				errChan {{.ChanSign}} err
 				return
