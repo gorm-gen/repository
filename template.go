d63ac8c3f2f8bcb2ab7329e9703d7d43fb508b17
wangben <wangben-1203@qq.com>
2025-06-19 09:48:25 +0800

    m

--- template.go
+++ template.go
@@ -2404,28 +2404,16 @@ func (d *_shardingDelete) Do(ctx context.Context) (int64, map[{{.ShardingKeyType
 	if _lenSharding == 0 {
 		return 0, nil, nil
 	}
-	dq := d.core.q.{{.StructName}}
-	if d.tx != nil {
-		dq = d.tx.{{.StructName}}
-	}
-	if d.qTx != nil {
-		dq = d.qTx.{{.StructName}}
-	}
+	qTx := d.qTx
 	var innerTx *query.QueryTx
 	var cancel context.CancelFunc
 	if d.tx == nil && d.qTx == nil {
 		innerTx = d.core.q.Begin()
-		dq = innerTx.{{.StructName}}
+		qTx = innerTx
 		ctx, cancel = context.WithCancel(ctx)
 		defer cancel()
 	}
-	var conditions []gen.Condition
-	if _len := len(d.conditionOpts); _len > 0 {
-		conditions = make([]gen.Condition, 0, _len)
-		for _, opt := range d.conditionOpts {
-			conditions = append(conditions, opt(d.core))
-		}
-	}
+	_condLen := len(d.conditionOpts)
 	sm := sync.Map{}
 	wg := sync.WaitGroup{}
 	errChan := make(chan error)
@@ -2444,25 +2432,21 @@ func (d *_shardingDelete) Do(ctx context.Context) (int64, map[{{.ShardingKeyType
 				{{.ChanSign}}d.worker
 			}()
 			defer wg.Done()
-			_conditions := make([]gen.Condition, len(conditions))
-			copy(_conditions, conditions)
-			_conditions = append(_conditions, Condition{{.ShardingKey}}(sharding)(d.core))
-			dr := dq.WithContext(ctx)
-			if d.unscoped {
-				dr = dr.Unscoped()
-			}
-			if len(d.scopes) > 0 {
-				dr = dr.Scopes(d.scopes...)
-			}
-			res, err := dr.Where(_conditions...).Delete()
+			_conditionOpts := make([]ConditionOption, _condLen, _condLen+1)
+			copy(_conditionOpts, d.conditionOpts)
+			_conditionOpts = append(_conditionOpts, ConditionSharding(sharding))
+			rows, err := d.core.Delete().
+				Tx(d.tx).
+				QueryTx(qTx).
+				Unscoped(d.unscoped).
+				Scopes(d.scopes...).
+				Where(_conditionOpts...).
+				Do(ctx)
 			if err != nil {
-				if {{.RepoPkgName}}.IsRealErr(err) {
-					d.core.logger.Error(fmt.Sprintf("【{{.StructName}}.ShardingDelete.%{{.ShardingKeyTypeFormat}}】失败", sharding), zap.Error(err), zap.ByteString("debug.Stack", debug.Stack()))
-				}
-				errChan {{.ChanSign}} err
+				errChan <- err
 				return
 			}
-			sm.Store(sharding, res.RowsAffected)
+			sm.Store(sharding, rows)
 			return
 		}(sharding)
 	}
